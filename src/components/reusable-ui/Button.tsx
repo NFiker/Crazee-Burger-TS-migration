a3987611771f52fb3dba7958bff489a25e2a9f6e
nasredine kherfi <nasredine.kherfi@oclock.school>
2025-03-29 02:23:33 +0100

    ♻️ F02 : import aliases

--- src/components/reusable-ui/Button.tsx
+++ src/components/reusable-ui/Button.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { theme } from "../../theme/theme";
+import { theme } from "@/theme/theme";
 import { css } from "styled-components";
 import { ComponentProps } from "react";
 
--- src/components/reusable-ui/Sticker.tsx
+++ src/components/reusable-ui/Sticker.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { theme } from "../../theme/theme";
+import { theme } from "@/theme/theme.ts";
 
 type StickerProps = {
   label: string;
