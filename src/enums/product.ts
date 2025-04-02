e78738ef2bfbd582b084cf777afb2324ef001357
nasredine kherfi <nasredine.kherfi@oclock.school>
2025-04-02 12:53:53 +0200

    ♻️ TS-06: re refacto ENUMS > product.ts

--- src/enums/product.ts
+++ src/enums/product.ts
@@ -1,32 +1,16 @@
-// export const EMPTY_PRODUCT = Object.freeze({
-//   id: "",
-//   title: "",
-//   imageSource: "",
-//   price: 0,
-//   isAvailable: true,
-//   isPublicised: false,
-// });
+import { MenuProduct } from "@/types/Product";
 
-// export const IMAGE_COMING_SOON = "/images/coming-soon.png";
-// export const IMAGE_NO_STOCK = "/images/stock-epuise.png";
-
-// export const BASKET_MESSAGE = {
-//   EMPTY: "Votre commande est vide.",
-//   LOADING: "Chargement en cours...",
-//   NOT_AVAILABLE: "Non disponible",
-// };
-
-export const EMPTY_PRODUCT = {
+export const EMPTY_PRODUCT: MenuProduct = {
   id: "",
   title: "",
   imageSource: "",
   price: 0,
   isAvailable: true,
   isPublicised: false,
-} as const;
+};
 
-export const IMAGE_COMING_SOON = "/images/coming-soon.png" as const;
-export const IMAGE_NO_STOCK = "/images/stock-epuise.png" as const;
+export const IMAGE_COMING_SOON = "/images/coming-soon.png";
+export const IMAGE_NO_STOCK = "/images/stock-epuise.png";
 
 export const BASKET_MESSAGE = {
   EMPTY: "Votre commande est vide.",
