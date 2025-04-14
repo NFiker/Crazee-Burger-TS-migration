d5c478b43d217a9f5d6241062709366ce8b67e26
nasredine kherfi <nasredine.kherfi@oclock.school>
2025-04-14 22:01:44 +0200

    ✨ BACKEND : config .env

---
+++ BACKEND/src/config/env.ts
@@ -0,0 +1,10 @@
+// src/config/env.ts
+import dotenv from "dotenv";
+dotenv.config();
+
+export const PORT = process.env.PORT || 3000;
+export const DATABASE_URL = process.env.DATABASE_URL as string;
+
+if (!DATABASE_URL) {
+  throw new Error("DATABASE_URL is missing in .env");
+}
