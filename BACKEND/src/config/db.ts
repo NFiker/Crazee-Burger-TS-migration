6b7d359afeb9cf736c4a93a880d8522829b788a8
nasredine kherfi <nasredine.kherfi@oclock.school>
2025-04-16 14:47:48 +0200

    ✨BACKEND : add schema to db export

--- BACKEND/src/config/db.ts
+++ BACKEND/src/config/db.ts
@@ -1,9 +1,11 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { DATABASE_URL } from "./env";
+import { schema } from "@/modules/user/user.schema"; // 👈 ajoute ceci
 
 const client = postgres(DATABASE_URL, {
-  ssl: true, // à adapter selon ton environnement (true en prod sur Railway, false en local)
+  ssl: true,
 });
 
-export const db = drizzle(client);
+// 👇 ici on passe le schéma
+export const db = drizzle(client, { schema });
