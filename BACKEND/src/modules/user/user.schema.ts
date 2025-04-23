7399d96d9d195cc0f3f9167bda35374a4f860f4f
nasredine kherfi <nasredine.kherfi@oclock.school>
2025-04-23 16:31:46 +0200

    ♻️  BACKEND > USER > menu price integer into numeric for float keeping

--- BACKEND/src/modules/user/user.schema.ts
+++ BACKEND/src/modules/user/user.schema.ts
@@ -1,4 +1,11 @@
-import { pgTable, serial, text, integer, boolean } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  serial,
+  text,
+  integer,
+  boolean,
+  numeric,
+} from "drizzle-orm/pg-core";
 
 // Table des utilisateurs
 export const users = pgTable("users", {
@@ -11,7 +18,7 @@ export const menuProducts = pgTable("menu_products", {
   userId: text("user_id").references(() => users.username), // FK vers users.username
   imageSource: text("image_source").notNull(),
   title: text("title").notNull(),
-  price: integer("price").notNull(),
+  price: numeric("price").notNull(),
   quantity: integer("quantity"), // optionnel
   isAvailable: boolean("is_available").notNull(),
   isPublicised: boolean("is_publicised").notNull(),
