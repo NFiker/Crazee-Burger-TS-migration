adb285f269bc8ed11f4637a5808b46a2ea3cfe29
nasredine kherfi <nasredine.kherfi@oclock.school>
2025-04-26 14:14:25 +0200

    ✨FRONTEND > ADDFORM > convert select options string to boolean for the bdd

--- FRONTEND/src/components/pages/order/main/mainRightSide/admin/adminPanel/addForm/AddForm.tsx
+++ FRONTEND/src/components/pages/order/main/mainRightSide/admin/adminPanel/addForm/AddForm.tsx
@@ -3,25 +3,31 @@ import { EMPTY_PRODUCT } from "@/constants/product.ts";
 import Form from "../form/Form.tsx";
 import SubmitButton from "./SubmitButton.js";
 import { useSuccessMessage } from "@/hooks/useSuccessMessage.ts";
-import { replaceFrenchCommaWithDot } from "@/utils/maths.ts";
+import {
+  replaceFrenchCommaWithDot,
+  sanitizeRawPriceInput,
+} from "@/utils/maths.ts";
 import { useParams } from "react-router-dom";
+import { convertStringToBoolean } from "@/utils/string.ts";
 
 export default function AddForm() {
-  //State
+  // State
   const { handleAdd, newProduct, setNewProduct } = useOrderContext();
   const { isSubmitted, displaySuccessMessage } = useSuccessMessage();
 
   const { username } = useParams();
 
-  //Comportements
+  // Comportements
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (!username) return;
+
     const newProductToAdd = {
       ...newProduct,
       id: crypto.randomUUID(),
-      price: replaceFrenchCommaWithDot(newProduct.price),
+      price: replaceFrenchCommaWithDot(newProduct.price), // clean le prix ici
     };
+
     handleAdd(newProductToAdd, username);
     setNewProduct(EMPTY_PRODUCT);
     displaySuccessMessage();
@@ -31,21 +37,23 @@ export default function AddForm() {
     event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
   ) => {
     const { name, value } = event.target;
-    setNewProduct({ ...newProduct, [name]: value });
+    let updatedValue: string | number | boolean = value;
+
+    if (name === "price") {
+      updatedValue = sanitizeRawPriceInput(value);
+    }
+    if (name === "isAvailable" || name === "isPublicised") {
+      updatedValue = convertStringToBoolean(value);
+    }
+
+    const updatedProduct = { ...newProduct, [name]: updatedValue };
+    setNewProduct(updatedProduct);
   };
 
-  //Affichage
+  // Affichage
   return (
     <Form product={newProduct} onSubmit={handleSubmit} onChange={handleChange}>
       <SubmitButton isSubmitted={isSubmitted} />
-      {/* <>
-        <Button
-          className="submit-button"
-          label={"Ajouter un nouveau produit au menu"}
-          version="success"
-        />
-        {isSubmitted && <SubmitMessage />}
-      </> */}
     </Form>
   );
 }
